379dab85f31c652430e39dd71fe8852c4fb9a769
Isaias Coronel Zapa <58237470+Isaias593@users.noreply.github.com>
2023-06-07 12:07:26 -0500

    Add files via upload

---
+++ Migrations/20230607132527_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CORONEL_ZAPA_V2.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreUser = table.Column<string>(type: "varchar(15)", nullable: false),
+                    Clave = table.Column<string>(type: "varchar(15)", nullable: false),
+                    estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ Models/HistorialAcceso.cs
@@ -0,0 +1,10 @@
+﻿namespace CORONEL_ZAPA_V2.Models
+{
+    public class HistorialAcceso
+    {
+        public int Id { get; set; }
+        public DateTime FechaHora{ get; set; }
+        public string UsuarioId { get; set; }
+        public Usuario Usuario { get;}
+    }
+}
